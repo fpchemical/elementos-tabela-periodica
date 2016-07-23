cf6b2b80a8d617f6a6d77d79cb55c30e1bc48aa6
Gabriel Kalani <gabrielsilva1956@gmail.com>
2016-07-23 13:45:21 -0300

    Update elementos.js

--- elementos.js
+++ elementos.js
@@ -1,270 +1,218 @@
-module.exports = { 
-	simbolo: 'H', 
-	nome: [
-		{
-			lingua: 'pt-br', 
-			valor: 'hidrogenio'
-		}
-	], 
-	familia: [
-		{
-			tipo: 'coluna',
-			valor: '1'
-		}, 
-		{
-			tipo: 'grupo',
-			valor: '1A'
-		}, 
-		{
-			tipo: 'descritivo',
-			valor: 'sem familia'
-		}
-	], 
-	periodo: 1, 
-	massaAtomica: 1.00797,
-	numero: {
-		eletrons: 1,
-		neutrons: 0,
-		protons: 1
-	},
-	distribuicaoEletronica: {
-		k: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		l: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		m: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		n: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		o: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		p: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		q: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-	},
-	camadaValencia: {
-		k: {
-				subnivel: 's',
-				eletrons: 1
-		}
-	},
-	propriedades: {
-		fisicas: [
-			{   nome: 'densidade',
-				valor: 0.0899, 
-				unidade: 'kg/m3'
-			},
+module.exports = {
+    simbolo: 'H',
+    nome: [{
+        lingua: 'pt-br',
+        valor: 'hidrogenio'
+    }],
+    familia: [{
+        tipo: 'coluna',
+        valor: '1'
+    }, {
+        tipo: 'grupo',
+        valor: '1A'
+    }, {
+        tipo: 'descritivo',
+        valor: 'sem familia'
+    }],
+    periodo: 1,
+    massaAtomica: 1.00797,
+    numero: {
+        eletrons: 1,
+        neutrons: 0,
+        protons: 1
+    },
+    distribuicaoEletronica: {
+        k: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        l: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        m: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        n: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        o: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        p: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        q: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+    },
+    camadaValencia: {
+        k: {
+            subnivel: 's',
+            eletrons: 1
+        }
+    },
+    propriedades: {
+        fisicas: [{
+                nome: 'densidade',
+                valor: 0.0899,
+                unidade: 'kg/m3'
+            },
 
-			{   nome: 'estadodaMateria',
-				valor: 'gasoso' 
-			},
-			{   nome: 'raioAtomico',
-				valor: 53,
-			}
-		],
-		quimicas:[ 
-			{   nome: 'meiaVida',
-				valor: 12.3, 
-				unidade: 'a'
-			},
-			{   nome: 'pontoFusao',
-				valor: {}
-			},
-			{   nome: 'pontoFusao',
-				valor: {}
-			},
-			{   nome: 'energiaIonização',
-				valor: 1312, 
-				unidade: 'kj/mol'
-			},
-			{   nome: 'estadoOxidacao',
-				valor: '1+' 
-			},
-			{   nome: 'estruturaCristalina',
-				valor: 'hexagonal', 
-				imagem: './hexagonal.png'
-			},
-			{   nome: 'raioCovalente',
-				valor: 12.3, 
-				unidade: 'pm'
-			},
-			{   nome: 'raioVanderWaals',
-				valor: 120, 
-				unidade: 'pm'
-			},
-			{   nome: 'entalpiadefusao',
-				valor: 0.05868, 
-				unidade: 'kJ/mol'
-			},
-			{   nome: 'entalpiadevaporizacao',
-				valor: 0.44936, 
-				unidade: 'kJ/mol'
-			},
-			{   nome: 'volumeMolar',
-				valor: 0.0000114, 
-				unidade: 'kJ/mol'
-			},
-			{
-				nome: 'pressaodeVapor',
-				valor: 209, 
-				unidade: 'Pa',
-				tipo: 'pressao'
-			},
-			{
-				nome: 'temperaturadeVapor',
-				valor: 23, 
-				unidade: 'K',
-				tipo: 'temperatura'
-			},
-			{   nome: 'calorEspecífico',
-				valor: 14304, 
-				unidade: 'J/(kg·K)'
-			},
-			{   nome: 'eletronegatividadePauling',
-				valor: 2.2, 
-				unidade: 'eV'
-			},
-			{   nome: 'condutividadeEletrica',
-				valor: 106, 
-				unidade: 'S/m'
-			},
-			{
-				nome: 'condutividadeTermica',
-				valor: 0.1815,
-				unidade: 'W/(m·K)'
-			}
-		]	
-	}
-},{
-	simbolo: 'Li', 
-	nome: [
-		{
-			lingua: 'pt-br', 
-			valor: 'Lítio'
-		}
-	], 
-	familia: [
-		{
-			tipo: 'coluna',
-			valor: '1'
-		}, 
-		{
-			tipo: 'grupo',
-			valor: '1A'
-		}
-	], 
-	periodo: 2, 
-	massaAtomica: 6,941,
-	numero: {
-		eletrons: 3,
-		neutrons: 0,
-		protons: 3
-	},
-	distribuicaoEletronica: {
-		k: [
-			{
-				subnivel: 's',
-				eletrons: 2
-			}
-		],
-		l: [
-			{
-				subnivel: 's',
-				eletrons: 1
-			}
-		],
-		m: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		n: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		o: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		p: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-		q: [
-			{
-				subnivel: 's',
-				eletrons: 0
-			}
-		],
-	},
-	camadaValencia: {
-		k: {
-				subnivel: 's',
-				eletrons: 1
-		}
-	},
-	propriedades: {
-		fisicas: [
-			{   nome: 'densidade',
-				valor: 0.534, 
-				unidade: 'g/cm³'
-			},
+            {
+                nome: 'estadodaMateria',
+                valor: 'gasoso'
+            }, {
+                nome: 'raioAtomico',
+                valor: 53,
+            }
+        ],
+        quimicas: [{
+            nome: 'meiaVida',
+            valor: 12.3,
+            unidade: 'a'
+        }, {
+            nome: 'pontoFusao',
+            valor: {}
+        }, {
+            nome: 'pontoFusao',
+            valor: {}
+        }, {
+            nome: 'energiaIonização',
+            valor: 1312,
+            unidade: 'kj/mol'
+        }, {
+            nome: 'estadoOxidacao',
+            valor: '1+'
+        }, {
+            nome: 'estruturaCristalina',
+            valor: 'hexagonal',
+            imagem: './hexagonal.png'
+        }, {
+            nome: 'raioCovalente',
+            valor: 12.3,
+            unidade: 'pm'
+        }, {
+            nome: 'raioVanderWaals',
+            valor: 120,
+            unidade: 'pm'
+        }, {
+            nome: 'entalpiadefusao',
+            valor: 0.05868,
+            unidade: 'kJ/mol'
+        }, {
+            nome: 'entalpiadevaporizacao',
+            valor: 0.44936,
+            unidade: 'kJ/mol'
+        }, {
+            nome: 'volumeMolar',
+            valor: 0.0000114,
+            unidade: 'kJ/mol'
+        }, {
+            nome: 'pressaodeVapor',
+            valor: 209,
+            unidade: 'Pa',
+            tipo: 'pressao'
+        }, {
+            nome: 'temperaturadeVapor',
+            valor: 23,
+            unidade: 'K',
+            tipo: 'temperatura'
+        }, {
+            nome: 'calorEspecífico',
+            valor: 14304,
+            unidade: 'J/(kg·K)'
+        }, {
+            nome: 'eletronegatividadePauling',
+            valor: 2.2,
+            unidade: 'eV'
+        }, {
+            nome: 'condutividadeEletrica',
+            valor: 106,
+            unidade: 'S/m'
+        }, {
+            nome: 'condutividadeTermica',
+            valor: 0.1815,
+            unidade: 'W/(m·K)'
+        }]
+    }
+}, {
+    simbolo: 'Li',
+    nome: [{
+        lingua: 'pt-br',
+        valor: 'Lítio'
+    }],
+    familia: [{
+        tipo: 'coluna',
+        valor: '1'
+    }, {
+        tipo: 'grupo',
+        valor: '1A'
+    }],
+    periodo: 2,
+    massaAtomica: 6,
+    941,
+    numero: {
+        eletrons: 3,
+        neutrons: 0,
+        protons: 3
+    },
+    distribuicaoEletronica: {
+        k: [{
+            subnivel: 's',
+            eletrons: 2
+        }],
+        l: [{
+            subnivel: 's',
+            eletrons: 1
+        }],
+        m: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        n: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        o: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        p: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+        q: [{
+            subnivel: 's',
+            eletrons: 0
+        }],
+    },
+    camadaValencia: {
+        k: {
+            subnivel: 's',
+            eletrons: 1
+        }
+    },
+    propriedades: {
+        fisicas: [{
+                nome: 'densidade',
+                valor: 0.534,
+                unidade: 'g/cm³'
+            },
 
-			{   nome: 'estadodaMateria',
-				valor: 'solido' 
-			},
-			{   nome: 'raioAtomico',
-				valor: 152,
-			}
-		],
-		quimicas:[]
-		}
-	}
-
-
-	
-
-
-
-
-
-
-
-
-
+            {
+                nome: 'estadodaMateria',
+                valor: 'solido'
+            }, {
+                nome: 'raioAtomico',
+                valor: 152,
+            }
+        ],
+        quimicas: []
+    }
+}
