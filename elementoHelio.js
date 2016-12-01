cddea604ef509e136be30d7ca8f6135eed382d67
FranFran <fpchemistry@gmail.com>
2016-12-01 03:51:50 -0200

    helio

---
+++ elementoHelio.js
@@ -0,0 +1,217 @@
+module.exports = { 
+	simbolo: 'He', 
+	nome: [
+		{
+			lingua: 'pt-br', 
+			valor: 'helio'
+		}
+	], 
+	familia: [
+		{
+			tipo: 'coluna',
+			valor: '18'
+		}, 
+		{
+			tipo: 'grupo',
+			valor: '8A'
+		}, 
+		{
+			tipo: 'descritivo',
+			valor: 'gases nobres'
+		}
+	], 
+	periodo: 1, 
+	massaAtomica: {
+		valor: 4.002602,
+		unidade: 'u'
+	},
+	numero: {
+		eletrons: 2,
+		neutrons: 2,
+		protons: 2
+	},
+	distribuicaoEletronica: {
+		k: [
+			{
+				s: 2
+			}
+		],
+		l: [
+			{
+				subnivel: 's',
+				eletrons: 0
+			},
+			{
+				subnivel: 'p',
+				eletrons: 0
+			}
+		],
+		m: [
+			{
+				subnivel: 's',
+				eletrons: 0
+			}, 
+			{
+				subnivel: 'p',
+				eletrons: 0
+			},
+			{
+				subnivel: 'd',
+				eletrons: 0
+			}
+		],
+		n: [
+			{
+				subnivel: 's',
+				eletrons: 0, 
+				subnivel: 'p',
+				eletrons: 0,
+				subnivel: 'd',
+				eletrons: 0,
+				subnivel: 'f',
+				eletrons: 0
+			}
+		],
+		o: [
+			{
+				subnivel: 's',
+				eletrons: 0, 
+				subnivel: 'p',
+				eletrons: 0,
+				subnivel: 'd',
+				eletrons: 0,
+				subnivel: 'f',
+				eletrons: 0
+			}
+		],
+		p: [
+			{
+				subnivel: 's',
+				eletrons: 0, 
+				subnivel: 'p',
+				eletrons: 0,
+				subnivel: 'd',
+				eletrons: 0
+			}
+		],
+		q: [
+			{
+				subnivel: 's',
+				eletrons: 0, 
+				subnivel: 'p',
+				eletrons: 0
+			}
+		],
+	},
+	camadaValencia: {
+		k: {
+				subnivel: 's',
+				eletrons: 2
+		}
+	},
+	propriedades: {
+		fisicas: [
+			{   nome: 'densidade',
+				valor: 0.1785, 
+				unidade: 'kg/m3'
+			},
+
+			{   nome: 'estadodaMateria',
+				valor: 'gasoso' 
+			},
+			{   nome: 'raioAtomico',
+				valor: 31,
+				unidade: 'pm'
+			}
+		],
+		quimicas:[ 
+			{   nome: 'meiaVida',
+				valor: 0, 
+				unidade: 'a'
+			},
+			{   nome: 'pontoFusao',
+				valor: 0.95,
+				unidade: 'k'
+
+			},
+			{   nome: 'pontoEbulicao'
+				valor: 4.22,
+				unidade: 'k'
+			},
+			{   nome: 'energiaIonização',
+				valor: 2372.3, 
+				unidade: 'kj/mol'
+			},
+			{   nome: 'estadoOxidacao',
+				valor: '0' 
+			},
+			{   nome: 'estruturaCristalina',
+				valor: 'hexagonal', 
+				imagem: './hexagonal.png'
+			},
+			{   nome: 'raioCovalente',
+				valor: 32, 
+				unidade: 'pm'
+			},
+			{   nome: 'raioVanderWaals',
+				valor: 140, 
+				unidade: 'pm'
+			},
+			{   nome: 'entalpiadefusao',
+				valor: 5.23, 
+				unidade: 'kJ/mol'
+			},
+			{   nome: 'entalpiadevaporizacao',
+				valor: 0,0845, 
+				unidade: 'kJ/mol'
+			},
+			{   nome: 'volumeMolar',
+				valor: 0.0000021, 
+				unidade: 'm3/mol'
+			},
+			{
+				nome: 'pressaodeVapor',
+				valor: 100, 
+				unidade: 'Pa',
+				tipo: 'pressao'
+			},
+			{
+				nome: 'temperaturadeVapor',
+				valor: 1.23, 
+				unidade: 'K',
+				tipo: 'temperatura'
+			},
+			{   nome: 'calorEspecífico',
+				valor: 5193, 
+				unidade: 'J/(kg·K)'
+			},
+			{   nome: 'eletronegatividadePauling',
+				valor: 0, 
+				unidade: 'eV'
+			},
+			{   nome: 'condutividadeEletrica',
+				valor: 0, 
+				unidade: 'S/m'
+			},
+			{
+				nome: 'condutividadeTermica',
+				valor: 0.152,
+				unidade: 'W/(m·K)'
+			}
+		]	
+	}
+}
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
